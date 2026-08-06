f9012e686abda0542079361d2ce13d8e7cfa1ce8
Mooncakk <85755431+Mooncakk@users.noreply.github.com>
2026-08-06 13:49:00 +0200

    refactoring: - supprime le schema public - modifie le nom de la base de donnée

--- sql/ddl/schema.sql
+++ sql/ddl/schema.sql
@@ -1,3 +1,6 @@
-CREATE OR ALTER schema health_app.raw;
-CREATE OR ALTER schema health_app.staging;
-CREATE OR ALTER schema health_app.common;
+--Création des schemas
+DROP SCHEMA IF EXISTS health_app_db.public;
+
+CREATE OR ALTER schema health_app_db.raw;
+CREATE OR ALTER schema health_app_db.staging;
+CREATE OR ALTER schema health_app_db.common;
