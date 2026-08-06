9966bb5d67a0399649cc55a7d324b07bb3cf91b4
Mooncakk <85755431+Mooncakk@users.noreply.github.com>
2026-08-06 15:32:24 +0200

    refactoring : modifie les noms de fichier sql pour la création des tables, le warehouse, la database et le role

--- scripts/devops_queries.sh
+++ scripts/devops_queries.sh
@@ -1,13 +1,13 @@
 
 snow --config-file ./connection/config.toml sql \
-  -f sql/ddl/raw_tables.sql \
-  -f sql/ddl/staging_tables.sql \
-  -f sql/ddl/common_tables.sql \
+  -f sql/ddl/tables_raw.sql \
+  -f sql/ddl/tables_staging.sql \
+  -f sql/ddl/tables_common.sql \
   -f sql/ddl/file_format.sql \
   -f sql/ddl/internal_stage.sql \
   -f sql/ddl/functions.sql \
   -f sql/ddl/procedures.sql \
   -f sql/ddl/stream.sql \
-  --warehouse ADA_COMPUTE \
-  --database HEALTH_APP \
-  --role dev_ops_role
+  --warehouse HEALTH_APP_WH \
+  --database HEALTH_APP_DB \
+  --role dev_ops_rl
