3daa7f106f1ef0d0c78e69d6ba04c2fe41feb0f0
Mooncakk <85755431+Mooncakk@users.noreply.github.com>
2026-08-07 15:05:21 +0200

    refactoring : renomme le nom du fichier et supprime le script app_rl_queries.sh

--- setup_script.sh
+++ setup_script.sh
@@ -6,6 +6,4 @@ snow --config-file ./connection/config.toml sql -f sql/rbac/roles_creation.sql &
 
 sh scripts/devops_queries.sh && \
 
-snow --config-file ./connection/config.toml sql -f sql/rbac/grant_truncate.sql && \
-
-sh scripts/app_rl_queries.sh
+snow --config-file ./connection/config.toml sql -f sql/rbac/grant_truncate.sql
