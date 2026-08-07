20e73d0287003da7017304ea5e2783be89e567ec
Mooncakk <85755431+Mooncakk@users.noreply.github.com>
2026-08-07 11:27:30 +0200

    refactoring : modifie la base de données, le role et le warehouse

--- scripts/app_rl_queries.sh
+++ scripts/app_rl_queries.sh
@@ -1,16 +1,16 @@
 #Chargement du fichier dans le stage
 snow --config-file ./connection/config.toml \
   stage copy ./dataset/HealthApp_2k.log @common.internal_stage \
-  --warehouse ADA_COMPUTE \
-  --database HEALTH_APP \
-  --role app_role && \
+  --warehouse HEALTH_APP_WH \
+  --database HEALTH_APP_DB \
+  --role app_rl && \
 
 #Création des taches et finalizer
 #Insertion des données
 snow --config-file ./connection/config.toml sql \
   -f sql/ddl/tasks.sql \
-  -f ./sql/dml/insert_into.sql \
-  --warehouse ADA_COMPUTE \
-  --database HEALTH_APP \
-  --role app_role
+  -f ./sql/dml/copy_into.sql \
+  --warehouse HEALTH_APP_WH \
+  --database HEALTH_APP_DB \
+  --role app_rl
 
